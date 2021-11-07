84a024c90db6a3f63a11c8e59da55eab9e0aa487
Alexander Brown <brownaa@lafayette.edu>
2021-11-07 15:47:05 -0500

    added display of values

--- Front_Susp.js
+++ Front_Susp.js
@@ -219,7 +219,7 @@ wheelslider.oninput = function(){
 rackslider.oninput = function(){
   rackdisp = this.value/1000.0;
   //print("wheel pos udpate: " +str(this.value))
-  document.getElementById("racksliderval").innerHTML = str(rackdisp)
+  document.getElementById("racksliderval").innerHTML = str(rackdisp/suspConfig.steering_ratio)
 }
 
 //callbacks for these functions:
